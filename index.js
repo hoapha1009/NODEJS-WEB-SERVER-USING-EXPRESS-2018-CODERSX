73d71ed680ff80d434ce5a57ba6ac410c2692c34
HoangPham <hoapha1009@gmail.com>
2020-04-17 21:12:34 +0700

    Finish bai10, bai11 and bai12

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var userRoute = require('./routes/user.route');
 var port = 3000;
 
 var app = express();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>res.render('index', {
     name: 'Hoang'
