d1c43f743d3b828021138e72e6b6837c5e16c8cf
HoangPham <hoapha1009@gmail.com>
2020-04-17 15:50:43 +0700

    Finish bai9 -Controller (MVC)

---
+++ controllers/user.controller.js
@@ -0,0 +1,37 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = (req, res)=>res.render('users/index', {
+    users: db.get('users').value()
+});
+
+module.exports.search = (req, res)=>{
+    var q = req.query.q;
+    var users = db.get('users').value();
+    var matchedUser = users.filter(user=>user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+
+    res.render('users/index', {
+        users: matchedUser,
+        inputValue: q
+    })
+};
+
+module.exports.create = (req, res)=>{
+    res.render('users/create');
+};
+
+module.exports.get = (req, res)=>{
+    var id = req.params.id;
+    
+    var user = db.get('users').find({ id: id }).value();
+
+    res.render('users/view', {
+        user: user
+    })
+};
+
+module.exports.postCreate = (req, res)=>{
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+};
